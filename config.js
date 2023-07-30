f0c39238cf8465342ef95091ec5fec6b1910d590
Royster <os@royster.email>
2023-07-31 00:14:00 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Roysterの站点监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2222636-872d54258ac4e72fe00b6b4e',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '导航页',
+      url: 'https://iuoyt.com/'
+    },
+    {
+      text: '博客站',
+      url: 'https://blog.iuoyt.com/'
+    },
+  ],
+};
